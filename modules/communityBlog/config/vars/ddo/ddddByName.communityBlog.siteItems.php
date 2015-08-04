8e241de4e5c903e82cb0d40348a31e5c791d9d97
masted <masted311@gmail.com>
2015-08-04 07:57:28 +0300

    Auto-commit on 04.08.2015 08:57:28

--- modules/communityBlog/config/vars/ddo/ddddByName.communityBlog.siteItems.php
+++ modules/communityBlog/config/vars/ddo/ddddByName.communityBlog.siteItems.php
@@ -3,6 +3,6 @@
 return [
   'title' => '($v ? `<h2>`.$v.`</h2>` : ``)',
   'images' => 'St::enum($v, ``, `"<a href=/".UPLOAD_DIR.\\`/\\`.$v." class=\\`thumb lightbox\\`><img src=/".UPLOAD_DIR."/".Misc::getFilePrefexedPath($v, \\"sm_\\")."></a>"`)',
-  'text' => '$v',
+  'text' => '`<a href="`.$pagePath.`/item/`.$id.`" class="dgray">`.$v.`</a>`',
   'dateCreate' => '$v ? Date::datetimeStrSql($v) : ``',
 ];
