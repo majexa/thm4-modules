01f217785c62b55fe359db4d146d2f49c43acbcd
masted <masted311@gmail.com>
2015-08-19 17:31:21 +0300

    Auto-commit on 19.08.2015 18:31:21

--- modules/afisha/tpl/afisha/list.php
+++ modules/afisha/tpl/afisha/list.php
@@ -9,8 +9,14 @@
 <script>
   <? if ($d['mobile']) { ?>
   new Element('div', {'class': 'title'}).inject(document.getElement('.header .container'));
+  Ngn.FramesSlider.MapContents.Thm = new Class({
+      Extends: Ngn.FramesSlider.MapContents,
+      _getSourceHtml: function(frameN, sourceSelector, force) {
+          return this.parent(frameN, sourceSelector, force).replace(/(<([^>]+)>)/ig, '');
+      }
+  });
   new Ngn.DdoTitleSlider(
-    new Ngn.FramesSlider.MapContents(document, document.getElement('.pages'), {
+    new Ngn.FramesSlider.MapContents.Thm(document, document.getElement('.pages'), {
       frameCssClass: 'cBody',
       map: {
         '.bookmarks': '.header .container .title'
