172f478493f0e2c95ae482902aed453b768f3046
masted <masted311@gmail.com>
2015-10-25 20:32:16 +0300

    Auto-commit on 25.10.2015 20:32:15

--- modules/afisha/lib/AfishaRouter.class.php
+++ modules/afisha/lib/AfishaRouter.class.php
@@ -3,7 +3,6 @@
 class AfishaRouter extends ThmFourRouter {
 
   function _getController() {
-    if (ThmFourRouter::isMobile()) return new CtrlMobileAfishaDefault($this);
     return new CtrlAfishaDefault($this);
   }
 
